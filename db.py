b37b8d663c2e1f62a8d778fdef7b36b211f01750
Ahmad Ihda Falah Annur <123763623+Falah21@users.noreply.github.com>
2025-12-21 11:06:07 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,51 +1,3 @@
-# # from pymongo import MongoClient
-# # import streamlit as st
-
-# # MONGODB_URI = "mongodb+srv://ahmadihdafalah:amdryzen1010@cluster0.10a3ead.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-# # DB_NAME = "surat_pal"
-
-# # client = MongoClient(MONGODB_URI)
-# # db = client[DB_NAME]
-
-
-# # surat_mess = db["surat_mess"]
-# # surat_container = db["surat_container"]
-# # surat_kantor = db["surat_kantor"]
-# # surat_lahan = db["surat_lahan"]
-# # surat_kuasa = db["surat_kuasa"]
-
-# # db.py
-# import os
-# from pymongo import MongoClient
-# from datetime import datetime
-
-# # =============================
-# # KONEKSI MONGODB
-# # =============================
-
-# def get_database():
-#     """
-#     Mengembalikan database surat_pal
-#     """
-#     MONGO_URI = os.getenv(
-#         "MONGO_URI",
-#         "mongodb://ahmadihdafalah:amdryzen1010@cluster0.10a3ead.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-#     )
-
-#     client = MongoClient(MONGO_URI)
-#     return client["surat_pal"]
-
-# def insert_mess_data(data: dict):
-#     """
-#     Simpan data sewa mess ke collection mess
-#     """
-#     db = get_database()
-#     collection = db["mess"]
-
-#     data["created_at"] = datetime.now()
-
-#     collection.insert_one(data)
-
 import streamlit as st
 from pymongo import MongoClient
 
@@ -72,4 +24,5 @@ def get_lahan_collection():
 
 def get_rumah_dinas_collection():
     return get_db()["rumah_dinas"]
+
 
